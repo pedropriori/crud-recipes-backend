7716b7f5604d80b9822af7ff163d611ffa9040d9
Pedro Priori <kadilo1@hotmail.com>
2023-05-30 15:32:11 -0300

    fix: server use's

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./server")
+const { server } = require("./server")
 
 const port = 8080
 
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const usersRoutes = require("./routes/users")
+const logger = require("./middleware/logger")
+
 const server = express()
+
+server.use(express.json())
+server.use(logger)
+server.use(usersRoutes.router)
 
 module.exports = { server }
