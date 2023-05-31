4205e8792b45ffe4be24b7045e45bca85962b54a
Pedro Priori <kadilo1@hotmail.com>
2023-05-31 15:59:56 -0300

    add recipe route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const usersRoutes = require("./routes/users")
+const recipesRoutes = require("./routes/recipes")
 const logger = require("./middleware/logger")
 
 const server = express()
@@ -7,5 +8,6 @@ const server = express()
 server.use(express.json())
 server.use(logger)
 server.use(usersRoutes.router)
+server.use(recipesRoutes.router)
 
 module.exports = { server }
