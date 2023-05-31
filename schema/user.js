f844561a54abe73e24677830618be01d20c8c498
Pedro Priori <kadilo1@hotmail.com>
2023-05-31 16:02:18 -0300

    feat: login schema

--- schema/user.js
+++ schema/user.js
@@ -20,6 +20,22 @@ const UserSchema = z.object({
     //.regex(new RegExp('.*[`~<>?,./!@#$%^&*()\\-_+="\'|{}\\[\\];:\\\\].*'), "One special character"),
 });
 
+const LoginSchema = z.object({
+  email: z.string({
+    required_error: "Description must be required",
+  }).email("Invalid email."), 
+  
+  password: z.string({
+    required_error: "Password must be required",
+  })
+    //.min(8, "Must be at least 8 characters in length")
+    //.regex(new RegExp('.*[A-Z].*'), "One uppercase character")
+    //.regex(new RegExp('.*[a-z].*'), "One lowercase character")
+    //.regex(new RegExp('.*[0-9].*'), "One number")
+    //.regex(new RegExp('.*[`~<>?,./!@#$%^&*()\\-_+="\'|{}\\[\\];:\\\\].*'), "One special character"),
+});
+
 module.exports = {
-  UserSchema
+  UserSchema,
+  LoginSchema
 }
